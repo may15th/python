c83445e817d8a9917f020a190a6ac0e3f14f8efb
고유한 <kyhan1563@naver.com>
2023-07-14 10:29:14 +0900

    py_02

--- test2.py
+++ test2.py
@@ -28,3 +28,56 @@ age = 30
 print(f'나이는 내년에 {age+1}이 됩니다.')
 d = {'name' : '홍길동', 'age' : 30}
 print(f'나의 이름은 {d["name"]}입니다. 나이는{d["age"]}입니다.')
+print(f'{"h1":<10}')
+print(f'{"hi":>10}')
+print(f'{"hi":-^10}')
+print(f'{"hi":!<10}')
+y = 3.4121223
+print(f'{y:0.4f}')
+print(f'{y:10.4f}')
+print(f'{{y}}')
+a='hobby'
+print(a.count('b'))
+a = "Python is the best choice"
+a.find('b')
+a.find('k')
+
+a = "Life is too short"
+print(a.index('t'))
+
+print(",".join('abcd'))
+a = "hi"
+print(a.upper())
+
+a="HI"
+print(a.lower())
+
+a="    hi    "
+print(a.lstrip())
+print(a.rstrip())
+print(a.strip())
+a = "life is too short"
+print(a.replace("life", "your leg"))
+
+a= "life is too short"
+b = a.split()
+print(b)
+b= "a,b,c,d"
+print(b.split(','))
+odd = [1,3,5,7,9]
+
+a = [1,2,3]
+print(a)
+print(a[0])
+
+a = [1,2,['a','b',['life','is']]]
+print(a[2][2][0])
+print(a[-1][-1][-2])
+
+a = [1,2,3,['a','b','c'],4,5]
+print(a[2:5])
+print(a[3][:2])
+
+ a= [1,2,3]
+ b=
+# 2-3 리스트 자료형
